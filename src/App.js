02f7d9eb80f07312638b2a4f56b302a5fa1b13d2
unknown <curry2478@naver.com>
2025-12-10 21:40:01 +0900

    feat: 라우팅 보호 기능 추가 (Protected Route)

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import Home from './pages/Home';
 import Popular from './pages/Popular';
 import Search from './pages/Search';
 import Wishlist from './pages/Wishlist';
+import { isLoggedIn } from './utils/storage';
 
 // 환경 변수에 설정된 TMDB API 키를 콘솔에 출력 (개발용)
 console.log('API Key:', process.env.REACT_APP_TMDB_API_KEY);
+
+// Protected Route 컴포넌트
+function ProtectedRoute({ children }) {
+  if (!isLoggedIn()) {
+    return <Navigate to="/signin" replace />;
+  }
+  return children;
+}
 
 function App() {
   return (
@@ -16,10 +25,38 @@ function App() {
       <Header />
       <Routes>
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/popular" element={<Popular />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/wishlist" element={<Wishlist />} />
+        <Route 
+          path="/" 
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/popular" 
+          element={
+            <ProtectedRoute>
+              <Popular />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/search" 
+          element={
+            <ProtectedRoute>
+              <Search />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/wishlist" 
+          element={
+            <ProtectedRoute>
+              <Wishlist />
+            </ProtectedRoute>
+          } 
+        />
         {/* 존재하지 않는 경로는 홈으로 리다이렉트 */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
